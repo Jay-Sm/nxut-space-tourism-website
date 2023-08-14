05969fe6653fe853b0efe90371ed8f1ddf2301df
Jay Sm <jaysmailg14@gmail.com>
2023-08-14 11:11:39 -0400

    technology page completed

--- pages/technology.vue
+++ pages/technology.vue
@@ -6,21 +6,17 @@
     </div>
     <div class="flex items-center">
       <div class="flex flex-col gap-y-[2.25rem] mr-[4rem]">
-        <button class="tech-selection active">1</button>
-        <button class="tech-selection">2</button>
-        <button class="tech-selection">3</button>
+        <button @click="changeTech(1)" class="tech-selection" :class="{ active: techIndex === 1 }">1</button>
+        <button @click="changeTech(2)" class="tech-selection" :class="{ active: techIndex === 2 }">2</button>
+        <button @click="changeTech(3)" class="tech-selection" :class="{ active: techIndex === 3 }">3</button>
       </div>
       <div>
         <h3 class="font-barlow-condensed text-[16px]">THE TERMINOLOGY…</h3>
-        <h1 class="font-bellefair text-[56px] mt-1 mb-3">LAUNCH VEHICLE</h1>
-        <p class="text-[18px] w-[27.5rem]">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload
-          from Earth's surface
-          to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing
-          150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+        <h1 class="font-bellefair text-[56px] mt-1 mb-3">{{ techName }}</h1>
+        <p class="text-[18px] w-[27.5rem]">{{ techDesc }}</p>
       </div>
       <div class="ml-auto">
-        <img src="../assets/images/technology/image-launch-vehicle-portrait.jpg"
-          class="w-[29] h-[29.75] min-w-[28rem] min-h-[28.75] ml-3">
+        <img :src="techImg">
       </div>
     </div>
   </div>
@@ -36,6 +32,43 @@ async function changeBg() {
   bgElement.classList.add('bg-background-technology-desktop')
 }
 changeBg()
+
+const techIndex = ref(1)
+const techName = ref('Launch vehicle'.toUpperCase())
+const techDesc = ref(`A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!`)
+
+import data from "/data/data.json";
+import techImg1 from '/assets/images/technology/image-launch-vehicle-portrait.jpg'
+import techImg2 from '/assets/images/technology/image-spaceport-portrait.jpg'
+import techImg3 from '/assets/images/technology/image-space-capsule-portrait.jpg'
+const techImg = ref(techImg1)
+
+function changeTech(index) {
+  switch (index) {
+    case 1:
+      techIndex.value = index
+      techName.value = data.technology[index - 1].name.toUpperCase()
+      techDesc.value = data.technology[index - 1].description
+      techImg.value = techImg1
+      break;
+
+    case 2:
+      techIndex.value = index
+      techName.value = data.technology[index - 1].name.toUpperCase()
+      techDesc.value = data.technology[index - 1].description
+      techImg.value = techImg2
+      break;
+
+    case 3:
+      techIndex.value = index
+      techName.value = data.technology[index - 1].name.toUpperCase()
+      techDesc.value = data.technology[index - 1].description
+      techImg.value = techImg3
+      break;
+  }
+  // console.log(index - 1)
+}
+
 
 </script>
 
